3e12799901bf122d3ffeb1b508ac458e5fb17a10
Nikolah Pearce <npearce@oucs1528.otago.ac.nz>
2018-04-24 12:00:55 +1200

    test files added in

--- phones.py
+++ phones.py
@@ -5,23 +5,8 @@ import kdtree
     @author Nikolah, Kiri, Megan and Meg.
 """
 
-#Class Phone to store the x and y values 
-class Phone:
-    
-    def __init__(self, east_in, north_in):
-        self._east = east_in
-        self._north = north_in
-
-    def get_east(self):
-        return self._east
-    
-    def get_north(self):
-        return self._north
-    
 def main():
 
-    #Make a list of phones to store each point
-    phones = []
     #Make a list of point lists
     points2D = []
 
@@ -35,16 +20,7 @@ def main():
             continue
         else:
             points = inp.split(" ")
-            p = Phone(points[0], points[1])
             points2D.append(points)
-            phones.append(p)
-
-    #Print out all the points
-    """ for p in phones:
-    e = p.get_east()
-    n = p.get_north()
-    print("Phone set up. East:", e, "and North:", n)
-    """
 
     #Print out all the points in the 2D list
     for p in points2D:
@@ -52,23 +28,25 @@ def main():
         y = p[1]
         print("East:", y, "and North:", x)
 
-    tree = kdtree.create(points2D)
-    kdtree.visualize(tree)
-    b = tree.is_balanced
+    #tree = kdtree.create(points2D)
+    tree = kdtree.create(dimensions=2)
 
+    points_length = len(points2D)
+    i = 0
+    while i < points_length:
+        tree.add(points2D[i])
+        i+=1
+    kdtree.visualize(tree)
+    print(list(tree.inorder()))
+    #tree.search_knn([50, 25], 4)
+    pt = [40, 50]
+    #result = tree.search_nn(pt)
+        
+    b = tree.is_balanced
     if b:
         print("Balanced! :)")
     else:
         print("Not balanced :(")
 
-    print(list(tree.inorder()))
-    #tree.search_nn( (1, 2, 0) )
-
-"""
- It can provide the k nearest neighbours to a point by maintaining k current bests 
- instead of just one. A branch is only eliminated when k points have been found
- and the branch cannot have points closer than any of the k current bests.
- """
- 
 if __name__ == '__main__':
     main()
