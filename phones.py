be3ee270e8988a1a4830239790f8f0c3301762c7
Nikolah Pearce <peani371@student.otago.ac.nz>
2018-04-30 21:30:09 +1200

    Fixed smallest range bug after talking to Paul Crane

--- phones.py
+++ phones.py
@@ -75,32 +75,38 @@ def find_minimum_set(tree, max_phones, points):
     return min_set_points
 
 #
-# Takes a set of points and turns them into the smallest enclosing circle.
+# Loop through every point and check its K neighbours.
+# Pass these neighbours to the smallest circle method.
+# Record the radius of each circle created.
+# Return a list [[r1], [r2]] of these radii.
 #
-def find_smallest_circle(points):
-    '''
-    megiddo algorithm:
-    welzl algoirthm:
-    algorithm welzl: #PSEUDO CODE FROM WIKIPEDIA
-    
-    input: Finite sets P and R of points in the plane
-    output: Minimal disk enclosing P with R on the boundary, or undefined if no such disk exists
-    
-    if P is empty or |R| ≥ 3:
-        if the points of R are cocircular:
-            return the ball they determine
-        else:
-            return undefined
+def find_radii_set(tree, max_phones, points):
+    #Set the two arrays as arbitrarily the first check
+    n = points[0]
+    # 2D array [[node, dist], [node, dist]] of the knn of the point we are checking
+    #curr_nn_nodes = tree.search_knn(n, max_phones)
+    # 2D array [[x, y], [x, y]] of the points of these knn
+    #curr_points = pull_all_coordinates(curr_nn_nodes)
 
-    choose p in P (randomly and uniformly)
-    D := welzl(P - { p }, R)
-    if p is in D:
-        return D
-    return welzl(P - { p }, R ∪ { p })
-    '''
-    range = 0
+    all_r = []
 
-    return range
+    i = 0
+    while i < len(points):
+        curr_pt = points[i]
+        curr_nn_nodes = tree.search_knn(curr_pt, max_phones)
+        curr_points = pull_all_coordinates(curr_nn_nodes)
+
+        copy = curr_points[:]
+        c = smallestcircle.make_circle(copy)
+
+        #curr_r = c[2]
+        all_r.append(c)
+        
+        #all_r.append(curr_r)
+        i+=1
+
+    return all_r
+
 
 #
 # Main method runs all the input and methods called.
@@ -126,7 +132,7 @@ def main():
     for p in points:
         x = p[0]
         y = p[1]
-        #print("East:", y, "and North:", x)
+        #print("Telephone added at (", x, ", ", y, ")", sep="")
 
     #print(points)
 
@@ -136,32 +142,43 @@ def main():
         sys.exit(0)
 
     tree = kdtree.create(points)
-    #kdtree.visualize(tree)
-    #inord_tree = list(tree.inorder())
-    #print(inord_tree)
-
-    #print_if_balanced(tree)
-    pt = [125.13,122.56]
-    nn_node = tree.search_nn(pt)
-    print("Nearest Neighbour is:\n", nn_node, sep="")
+    '''
     minimum_nn_points = find_minimum_set(tree, max_phones, points)
-    print("\nThe minimum points OF EVERYTHING found:", minimum_nn_points, sep="\n")
+    print("\nOG METHOD.\nThe minimum points OF EVERYTHING found:", minimum_nn_points, sep="\n")
 
     #answer = find_smallest_circle(minimum_nn_points)
     #print("Range found:", answer)
 
     copy = minimum_nn_points[:]
 
-    #circle = smallestcircle()
     c = smallestcircle.make_circle(copy)
-    print("Range found\nx:", c[0], "y:", c[1], "r:", c[2])
+    print("OG METHOD.\nRange found\nx:", c[0], "y:", c[1], "r:", c[2])
 
     # Reduce by some arbitrarily small amount to go from min of 12 to max of at most 11
-    r = c[2]
-    true_r = (r - 0.1)
-    print("New radius found! Radius:", true_r)
+    rog = c[2]
+    true_rog = (rog - 0.1)
+    print("OG METHOD.\nNew radius found! Radius:", true_rog)
+    '''
+    
+    # list of all POSSIBLE radi
+    all_radii = find_radii_set(tree, max_phones, points)
+    min_r = all_radii[0]
 
+    #find the minimum radius
+    i = 0
 
+    while i < len(all_radii):
+        curr_r = all_radii[i]
+        if curr_r[2] < min_r[2]:
+            min_r = curr_r
+        i += 1
+    print("Smallest enlosing circle found!")
+    print("x:", min_r[0], "\ny:", min_r[1], "\nr:", min_r[2])
+
+    # Decrease minimum of 12 to get maximum of 11
+    max_eleven = (min_r[2] - 0.1)
+    print("\nGotta remove that 12th point though..\nRange now:", max_eleven)
+    print("\nMaximum range:", "%0.2f" % max_eleven, "metres")
 
 if __name__ == '__main__':
     main()
